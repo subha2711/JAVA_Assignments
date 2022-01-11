bc0edddced5aa6c7b26d2a1988ad2b032f3f5048
subha2711 <suba.pr2711@gmail.com>
2022-01-11 13:12:23 -0600

    assignment1

---
+++ src/assignment1/Name.java
@@ -0,0 +1,15 @@
+package assignment1;
+
+public class Name {
+
+	public static void main(String[] args) {
+		System.out.println("Hello !!!! I am " +name("subha"));
+
+	}
+	
+	public static String name(String name) {
+		
+		return name;
+	}
+
+}
---
+++ src/assignment2/User_Input.java
@@ -0,0 +1,10 @@
+package assignment2;
+
+public class User_Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
